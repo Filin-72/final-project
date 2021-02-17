d3465842571709e91f36666ecfc5dee8703d939e
Филипп Янов <yanovfilipp@mail.ru>
2021-02-17 18:32:35 +0300

    Первый шаг сделан!

---
+++ conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: ec or fr")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    browser_name = request.config.getoption("browser_name")
+    browser = webdriver.Chrome()
+    language = request.config.getoption('language')
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
