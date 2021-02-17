4fd9c5b13830549f13fa1b37bd622549ea5b0e78
Филипп Янов <yanovfilipp@mail.ru>
2021-02-18 00:57:14 +0300

    Добавлены итоговые версии файлов

--- test_main_page.py
+++ test_main_page.py
@@ -1,17 +1,14 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
-
-link = "http://selenium1py.pythonanywhere.com/"
-
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+from pages.basket_page import BasketPage
+import pytest
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    login_page = page.go_to_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
@@ -20,11 +17,17 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
+def test_guest_should_see_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page2 = LoginPage(browser, link)
+    page2.open()
+    page2.should_be_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-
+    page.should_enter_basket()
+    page2 = BasketPage(browser, browser.current_url)
+    page2.is_basket_empty()
+    page2.should_be_basket_empty_message()
